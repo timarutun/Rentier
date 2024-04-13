93f3c2ce884e639ad70400c698b1cfb85e99879f
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-04-12 22:34:29 -0400

    Initial Commit

---
+++ RentApp/RentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentAppApp.swift
+//  RentApp
+//
+//  Created by Timur on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
